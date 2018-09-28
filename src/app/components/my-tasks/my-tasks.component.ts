d0a2e0b83efc8e0faa46d72616cd922ab03ea89d
gowthamkumar <gowthamkumardoit@gmail.com>
2018-09-28 17:26:37 +0530

    Task page re-design

--- src/app/components/my-tasks/my-tasks.component.ts
+++ src/app/components/my-tasks/my-tasks.component.ts
@@ -31,17 +31,7 @@ export class MyTasksComponent implements OnInit {
   @ViewChild('delete')
   deleteModal;
   selectedValue: string;
-  @ViewChildren('pages')
-  pages: QueryList<any>;
-  itemsPerPage = 5;
-  numberOfVisiblePaginators = 10;
-  numberOfPaginators: number;
-  paginators: Array<any> = [];
-  activePage = 1;
-  firstVisibleIndex = 1;
-  lastVisibleIndex: number = this.itemsPerPage;
-  firstVisiblePaginator = 0;
-  lastVisiblePaginator = this.numberOfVisiblePaginators;
+
 
   istaskExists: boolean;
   errorMessage: string;
@@ -87,27 +77,9 @@ export class MyTasksComponent implements OnInit {
       { taskName: 'Task 4', companyName: 'Company 4', serviceName: 'Service 4', userName: 'User 4' },
       { taskName: 'Task 5', companyName: 'Company 5', serviceName: 'Service 5', userName: 'User 5' },
     ];
-    this.getPagination();
     this.showRightPanel = false;
   }
-  getPagination() {
-    setTimeout(() => {
-      this.paginators = [];
-      if (this.taskList.length % this.itemsPerPage === 0) {
-        this.numberOfPaginators = Math.floor(
-          this.taskList.length / this.itemsPerPage
-        );
-      } else {
-        this.numberOfPaginators = Math.floor(
-          this.taskList.length / this.itemsPerPage + 1
-        );
-      }
-
-      for (let i = 1; i <= this.numberOfPaginators; i++) {
-        this.paginators.push(i);
-      }
-    }, 200);
-  }
+ 
   closeDatePicker() {
     const element: HTMLCollection = document.getElementsByClassName('mydp');
     for (let i = 0; i < 10; i++) {
@@ -144,8 +116,6 @@ export class MyTasksComponent implements OnInit {
     });
   }
   openModal() {
-    // this.closeDatePicker();
-    //this.basicModal.show();
     this.showRightPanel = true;
     this.isEditTask = false;
 
@@ -201,84 +171,5 @@ export class MyTasksComponent implements OnInit {
     if (this.deletedItem) {
     }
   }
-  // Pagination code
-  changePage(event: any) {
-    if (
-      event.target.text >= 1 &&
-      event.target.text <= this.numberOfPaginators
-    ) {
-      this.activePage = +event.target.text;
-      this.firstVisibleIndex =
-        this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-      this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-    }
-  }
-
-  nextPage(event: any) {
-    // if (this.pages.last.nativeElement.classList.contains('active')) {
-    if (
-      this.numberOfPaginators - this.numberOfVisiblePaginators >=
-      this.lastVisiblePaginator
-    ) {
-      this.firstVisiblePaginator += this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator += this.numberOfVisiblePaginators;
-    } else {
-      this.firstVisiblePaginator += this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator = this.numberOfPaginators;
-    }
-    // }
-
-    this.activePage += 1;
-    this.firstVisibleIndex =
-      this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-  }
-
-  previousPage(event: any) {
-    //  if (this.pages.first.nativeElement.classList.contains('active')) {
-    if (
-      this.lastVisiblePaginator - this.firstVisiblePaginator ===
-      this.numberOfVisiblePaginators
-    ) {
-      this.firstVisiblePaginator -= this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator -= this.numberOfVisiblePaginators;
-    } else {
-      this.firstVisiblePaginator -= this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator -=
-        this.numberOfPaginators % this.numberOfVisiblePaginators;
-    }
-    //  }
-
-    this.activePage -= 1;
-    this.firstVisibleIndex =
-      this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-  }
-
-  firstPage() {
-    this.activePage = 1;
-    this.firstVisibleIndex =
-      this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-    this.firstVisiblePaginator = 0;
-    this.lastVisiblePaginator = this.numberOfVisiblePaginators;
-  }
-
-  lastPage() {
-    this.activePage = this.numberOfPaginators;
-    this.firstVisibleIndex =
-      this.activePage * this.itemsPerPage - this.itemsPerPage + 1;
-    this.lastVisibleIndex = this.activePage * this.itemsPerPage;
-
-    if (this.numberOfPaginators % this.numberOfVisiblePaginators === 0) {
-      this.firstVisiblePaginator =
-        this.numberOfPaginators - this.numberOfVisiblePaginators;
-      this.lastVisiblePaginator = this.numberOfPaginators;
-    } else {
-      this.lastVisiblePaginator = this.numberOfPaginators;
-      this.firstVisiblePaginator =
-        this.lastVisiblePaginator -
-        (this.numberOfPaginators % this.numberOfVisiblePaginators);
-    }
-  }
+  
 }
